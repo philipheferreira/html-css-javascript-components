4d82cc9e02f9a267e26ff5c1f8a9f0e564a91c60
philipheferreira <philipheferreira@gmail.com>
2024-08-22 12:18:35 -0300

    Feat: correct change image button

--- 1 - components/1 - buttons/4 - button change image/script.js
+++ 1 - components/1 - buttons/4 - button change image/script.js
@@ -6,10 +6,30 @@ let contador = 0;
 // botão
 let botaoImagem = document.querySelector(".botao");
 
-let mudar = () => {
 
-	console.log("Teste");
+let mudarImagem = (contado) => {
+
+	if (contado == 0){ 
+		document.querySelector(".imagem").src = "img/one_piece.png";
+	}else
+	if(contado == 1){
+		document.querySelector(".imagem").src = imagem1;
+	}else
+	if(contado ==2 ){
+		document.querySelector(".imagem").src = imagem2;
+	}
 }
 
+// função que atualiza contador e valida caso o contador chege a 3 e zera de novo. Depois essa função chama mudar imagem
+let mudar = () => {
+	contador++ 
+	console.log(contador);
+
+	if(contador == 3){
+		contador = 0;
+	}
+	mudarImagem(contador);
+}
 
 botaoImagem.addEventListener("click", mudar);
+
