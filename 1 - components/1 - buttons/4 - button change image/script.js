e63561f2206982e05d0b07f928641e98324a492f
philipheferreira <philipheferreira@gmail.com>
2024-08-22 11:39:35 -0300

    button change image basic template

---
+++ 1 - components/1 - buttons/4 - button change image/script.js
@@ -0,0 +1,15 @@
+//variaveis
+let imagem1 = "img/yu_yu_hakusho.png";
+let imagem2 = "img/zatch_bell.jpg";
+let contador = 0;
+
+// botão
+let botaoImagem = document.querySelector(".botao");
+
+let mudar = () => {
+
+	console.log("Teste");
+}
+
+
+botaoImagem.addEventListener("click", mudar);
