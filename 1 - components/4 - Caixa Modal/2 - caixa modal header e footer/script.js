e7042c001ff7d059ca9e1d3b3022c7efa5dbab69
philipheferreira <philipheferreira@gmail.com>
2024-10-10 15:27:25 -0300

    Feat: box model Header and footer - comands open and close in javascript

---
+++ 1 - components/4 - Caixa Modal/2 - caixa modal header e footer/script.js
@@ -0,0 +1,18 @@
+let modal = document.querySelector(".modal");
+
+let btnModal = document.querySelector(".meuBotao");
+
+let btnFechar = document.querySelector(".fechar");
+
+
+let abrirModal = () => {
+	modal.style.display = "block";
+}
+
+let fecharModal = () => {
+	modal.style.display = "none";
+}
+
+btnModal.addEventListener("click", abrirModal);
+
+btnFechar.addEventListener("click", fecharModal);
