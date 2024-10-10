1fefc973c3028b03e6818fe3cd37c022ee046ce0
philipheferreira <philipheferreira@gmail.com>
2024-10-10 14:34:39 -0300

    Feat: caixa modal finish

--- 1 - components/4 - Caixa Modal/script.js
+++ 1 - components/4 - Caixa Modal/script.js
@@ -6,13 +6,19 @@ let btnModal = document.querySelector(".meuBotao");
 let btnFechar = document.querySelector(".fechar");
 
 let abrirModal = () => {
-	modal.style.display = "block";
+	modal.style.display = "block"; // define o block no display modal tornandoos visiveis
 }
 
 let fecharModal = () => {
-	modal.style.display = "none";
+	modal.style.display = "none"; // define none no display modal tornando invisiveis
 }
 
 btnModal.addEventListener("click", abrirModal);
 
 btnFechar.addEventListener("click", fecharModal);
+
+window.addEventListener("click", function(){
+	if(event.target == modal){
+		modal.style.display = "none";
+	}
+});
