69fe100b67afc83438753ccc3cc2112291164d16
philipheferreira <philipheferreira@gmail.com>
2024-10-10 14:13:35 -0300

    Feat: modal box function open and close modal sucess

--- 1 - components/4 - Caixa Modal/script.js
+++ 1 - components/4 - Caixa Modal/script.js
@@ -1,12 +1,18 @@
-//pegando o modal
+
 let modal = document.querySelector(".modal");
 
-//pegando botão
 let btnModal = document.querySelector(".meuBotao");
 
+let btnFechar = document.querySelector(".fechar");
 
 let abrirModal = () => {
 	modal.style.display = "block";
 }
 
+let fecharModal = () => {
+	modal.style.display = "none";
+}
+
 btnModal.addEventListener("click", abrirModal);
+
+btnFechar.addEventListener("click", fecharModal);
