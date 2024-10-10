89b768738dbd4d1bfe92477f4ac5fb26d974fb53
philipheferreira <philipheferreira@gmail.com>
2024-10-10 19:09:49 -0300

    Feat: button show more or less finish

--- 1 - components/1 - buttons/12- botar ler muito ou ler menos/script.js
+++ 1 - components/1 - buttons/12- botar ler muito ou ler menos/script.js
@@ -1,1 +1,17 @@
-console.log("Ola")
+let pontinhos = document.querySelector(".pontinhos");
+let maisTexto = document.querySelector(".mais");
+let btnTexto = document.querySelector(".botao");
+
+let aparecerEdesaparecerTexto = () => {
+	if(pontinhos.style.display === "none"){
+		pontinhos.style.display = "inline";
+		btnTexto.innerHTML = "Ler mais";
+		maisTexto.style.display = "none"
+	}else{
+		pontinhos.style.display = "none";
+		btnTexto.innerHTML = "Ler menos";
+		maisTexto.style.display = "inline";
+	}
+}
+
+btnTexto.addEventListener("click", aparecerEdesaparecerTexto);
