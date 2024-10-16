4456e8a6853d863322ef07f687f248ee40aabab3
philipheferreira <philipheferreira@gmail.com>
2024-10-16 19:08:20 -0300

    Feat: finish box modal game

---
+++ 1 - components/4 - Caixa Modal/3 - caixa modal Game/script.js
@@ -0,0 +1,24 @@
+let modal = document.querySelector(".modal");
+
+let btnModal = document.querySelector(".meuBotao");
+
+let btnFechar = document.querySelector(".fechar");
+
+
+let abrirModal = () => {
+	modal.style.display = "block";
+}
+
+let fecharModal = () => {
+	modal.style.display = "none";
+}
+
+btnModal.addEventListener("click", abrirModal);
+
+btnFechar.addEventListener("click", fecharModal);
+
+window.addEventListener("click", function(){
+	if(event.target == modal){
+		modal.style.display = "none";
+	}
+})
