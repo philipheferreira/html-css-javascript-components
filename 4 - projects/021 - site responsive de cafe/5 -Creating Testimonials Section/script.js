1f40daebcb654ab6f04855cea4f4cf09f3961cfd
philipheferreira <philipheferreira@gmail.com>
2025-09-30 10:49:15 -0300

    Feat: little adjusts in page for coffe homepage

--- 4 - projects/021 - site responsive de cafe/5 -Creating Testimonials Section/script.js
+++ 4 - projects/021 - site responsive de cafe/5 -Creating Testimonials Section/script.js
@@ -6,5 +6,38 @@ abrirMenuBotao.addEventListener("click", () => {
 	document.body.classList.toggle("show-mobile-menu");
 });
 
-// Fechar menu quando o botão fehcar for clicado
+// Fechar menu quando o botão fechar for clicado
 fecharMenuBotao.addEventListener("click", () => abrirMenuBotao.click());
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-container', {
+	loop: true,
+	spaceBetween: 25,
+	grabCursor: true,
+	
+	// Caso precise de uma paginação
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true, // corrigido: era "dynamucBullets"
+	},
+	
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next', // corrigido: faltava o ponto
+		prevEl: '.swiper-button-prev'
+	},
+	
+	// Responsive BreakPoints
+	breakpoints: {
+		0: {
+			slidesPerView: 1
+		},
+		768: {
+			slidesPerView: 2
+		},
+		1024: {
+			slidesPerView: 3
+		}
+	}
+});
