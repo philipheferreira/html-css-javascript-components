9f37c1779fef97e00e430359c90cacdc0df6fa38
philipheferreira <philipheferreira@gmail.com>
2026-01-13 18:14:24 -0300

    Feat: adjust validation for operations

--- 3 - pages/028 - Form validation/script.js
+++ 3 - pages/028 - Form validation/script.js
@@ -1,10 +1,27 @@
+let infosFormulario = document.querySelector('.meuFormulario')
 let submitBotao = document.querySelector(".botaoSubmit")
 
-function funcaoSubmit(){
+let clickarAtivarSubmitDoFormulario = () => {
 
+	event.preventDefault(); /* Impede o comportamento padrao do formulario, que seria recarregar a pagina */
 
-	alert("Teste botao submit");
+	let primeiroNome = document.querySelector('.pnome').value;
+	let ultimoNome = document.querySelector('.unome').value;
+	let pais = document.querySelector('.paisNascimento').value;
+	let idade = document.querySelector('.idade').value;
+	let resumo = document.querySelector('.texto').value;
+	
+	const mensagem = `
+		--- Dados do Formulario ---
+		Primeiro nome: ${primeiroNome}.
+		Ultimo nome: ${ultimoNome}.
+		Pais: ${pais}
+		Idade: ${idade}
+		Resumo: ${resumo}
+	`;
+
+	alert(mensagem);
 }
 
 
-submitBotao.addEventListener("click", funcaoSubmit);
+infosFormulario.addEventListener("submit", clickarAtivarSubmitDoFormulario); // sera ativado toda vez que a paginar ativar o submit
