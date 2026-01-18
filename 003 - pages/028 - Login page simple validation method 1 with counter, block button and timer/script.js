ad53ce716200e0a60c8b7f7b2e23894f42e32fd2
philipheferreira <philipheferreira@gmail.com>
2026-01-18 09:35:52 -0300

    feat: create new project timer

---
+++ 003 - pages/028 - Login page simple validation method 1 with counter, block button and timer/script.js
@@ -0,0 +1,59 @@
+/*
+
+O meu codigo estava com o problema de nao redirecionar a pagina para a proxima depois de ser selecionada: 
+
+A Causa do Problema
+O seu código está funcionando, a lógica do if está correta, mas algo acontece logo depois que impede a mudança de página.
+
+O culpado é o comportamento padrão de um formulário.
+
+1.Você está usando uma tag <form> e um botão do type="submit".
+2.Quando você clica em um botão type="submit" dentro de um <form>, o comportamento padrão do navegador é enviar o formulário e recarregar a página.
+3.O seu JavaScript executa a função funcaoLogar(), mostra o alert e tenta mudar para home.html.
+4.Mas, imediatamente depois, o navegador executa a sua ação padrão: recarregar a página atual. Esse recarregamento anula a sua tentativa de redirecionamento.
+
+É a forma semanticamente correta. Funciona com o clique do mouse e com a tecla "Enter". É o padrão da indústria. 
+Requer uma pequena mudança no HTML e no JavaScript.
+
+
+
+ */
+
+let contador = 0;
+
+let logarBotao = document.querySelector(".logar");
+let campoLogin = document.querySelector(".login");
+let campoSenha = document.querySelector(".senha");
+
+function funcaoLogar(){
+
+	event.preventDefault();	/* A linha mais importante: impede o envio e o recarregamento da pagina */
+
+	let login = document.querySelector('.login').value
+	let senha = document.querySelector('.senha').value
+
+	if(login == "admin" && senha == "admin"){
+		alert('Bem vindo usuario admin, voce logou com sucesso.');
+		location.href = "pages/home.html";
+		
+	}else if(login != "admin" && senha != "admin" && contador >= 3){
+		alert(`Voce atingiu o limite de erros permitos da conta. foram ${contador}. Seu acesso foi bloqueado ` )
+
+		logarBotao.disabled = true;
+		campoLogin.value = "";
+		campoSenha.value = "";
+		campoLogin.disabled = true;
+		campoSenha.disabled = true;
+		logarBotao.value = "Bloqueado"
+
+	}
+
+	else{
+		contador++;
+		alert(`Usuario ou senha incorretos. voce errou ${contador} vezes, so tem mais ` + (3 - contador) + ' tentativas');
+		console.log(`Tentativa ${contador} de 3.`);
+	}
+
+}
+
+logarBotao.addEventListener("click", funcaoLogar);
