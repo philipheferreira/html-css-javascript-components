b4a23e5b97285c806593a953b9926256e41e9ca0
philipheferreira <philipheferreira@gmail.com>
2026-02-09 19:10:24 -0300

    Feat: corrections to do list project

---
+++ 004 - projects/010 - to do list/002 - to do list using list organization/script.js
@@ -0,0 +1,36 @@
+let taskInput = document.querySelector(".novaTarefa");
+let addButton = document.querySelector("button")[0];
+let incompleteTaskHolder = document.querySelector(".tarefasIncompletas");
+let completedTasksHolder = document.querySelector("tarefaCompleta");
+
+let criarNovaTarefaElemento = () => {
+
+}
+
+let adicionarTarefa = () => {
+
+}
+
+let editarTarefa = () => {
+
+}
+
+let deletarTarefa = () => {
+
+}
+
+let marcarTarefaComoCompleta = () => {
+
+}
+
+let tarefaIncompleta = () => {
+
+}
+
+let ajaxRequisicao = () => {
+
+}
+
+let bindTarefaEvento = () => {
+	
+}
