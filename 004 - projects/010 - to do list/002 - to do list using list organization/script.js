2d2efdd0e5dbe883355730f1248d0fb71411d9e7
philipheferreira <philipheferreira@gmail.com>
2026-02-10 13:30:19 -0300

    Things i forget

--- 004 - projects/010 - to do list/002 - to do list using list organization/script.js
+++ 004 - projects/010 - to do list/002 - to do list using list organization/script.js
@@ -1,18 +1,33 @@
-let taskInput = document.querySelector(".novaTarefa");
-let addButton = document.querySelector("button")[0];
-let incompleteTaskHolder = document.querySelector(".tarefasIncompletas");
-let completedTasksHolder = document.querySelector("tarefaCompleta");
+let tarefaInput = document.querySelector(".novaTarefa");
+let adicionarBotao = document.querySelector("button")[0];
+let segurarTarefaIncompleta = document.querySelector(".tarefasIncompletas");
+let segurarTarefaCompleta = document.querySelector("tarefaCompleta");
 
 let criarNovaTarefaElemento = () => {
+
+	let listaDeItens = document.createElement("li");
+
+	// input (checkbox)
+	let checkBox = document.createElement("input"); // checkbx
+
+	//label
+	let label = document.createElement("input"); // label
+
+	//input
+	let editInput = document.createElement("button");
+
+	// button
+
 
 }
 
 let adicionarTarefa = () => {
-
+	console.log(" Adicionar tarefa a lista de tarefas...");
 }
 
 let editarTarefa = () => {
-
+	console.log("Editar tarefa...");
+	
 }
 
 let deletarTarefa = () => {
@@ -20,15 +35,24 @@ let deletarTarefa = () => {
 }
 
 let marcarTarefaComoCompleta = () => {
+	console.log("Tarefa Completada...");
+
+
 
 }
 
 let tarefaIncompleta = () => {
-
+	console.log("Tarefa incompleta...");
+	// Marcar tarefa como incompleta.
+	// Quando o checkbox estiver desmarcado
+	// Append a lista de tarefas de item para o tasksIncompletas
+	let listaDeItens = this.parentNode;
+	segurarTarefaIncompleta.appendChild(listItem);
+	bindTarefaEvento(listaDeItens, marcarTarefaComoCompleta);
 }
 
 let ajaxRequisicao = () => {
-
+	console.log("AJAX Request");
 }
 
 let bindTarefaEvento = () => {
