d45338c68fde24bb0171eccc393be01db43017e9
philipheferreira <philipheferreira@gmail.com>
2026-02-10 10:48:13 -0300

    Feat: delete useless project to do list

---
+++ 004 - projects/010 - to do list/003 - Other simple way to make a to do list/script.js
@@ -0,0 +1,12 @@
+let inputtdl = document.querySelector('.textarea');
+let buttontdl = document.querySelector('.buttoninput');
+let listtdl = document.querySelector('.todolist');
+
+// inicializa todos do localStorage quando a pagina carregar
+document.addEventListener('DOMContentLoaded', loadTodoLists);
+
+// evento para escutar
+
+
+// funcao para handle botao click
+
