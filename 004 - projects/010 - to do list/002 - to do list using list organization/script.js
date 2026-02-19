100a09f24b14610138971ad86c223ae932ae457e
philipheferreira <philipheferreira@gmail.com>
2026-02-19 16:46:01 -0300

    Feat: create a new cap project for do

--- 004 - projects/010 - to do list/002 - to do list using list organization/script.js
+++ 004 - projects/010 - to do list/002 - to do list using list organization/script.js
@@ -3,26 +3,53 @@ let adicionarBotao = document.querySelector("button")[0];
 let segurarTarefaIncompleta = document.querySelector(".tarefasIncompletas");
 let segurarTarefaCompleta = document.querySelector("tarefaCompleta");
 
-let criarNovaTarefaElemento = () => {
+let criarNovaTarefaElemento = (TarefaRepassada) => {
 
-	let listaDeItens = document.createElement("li");
+	let listaDeItens = document.createElement("li"); // Cria o elemento li
 
 	// input (checkbox)
-	let checkBox = document.createElement("input"); // checkbx
+	let checkBox = document.createElement("input"); // Cria o elemento input
 
 	//label
-	let label = document.createElement("input"); // label
+	let label = document.createElement("input"); // Cria o elemento
 
-	//input
-	let editInput = document.createElement("button");
+	//input (text)
+	let editarInput = document.createElement("button"); // Cria o elemento do tipo botao
 
-	// button
+	// button.edit
+	let botaoEditar = document.createElement("button"); // Cria o elemento do tipo botao
 
+	// button.delete
+	let botaoDeletar = document.createElement("button"); // Cria o elemento do tipo botao
+
+
+	label.innerText = TarefaRepassada;
+
+	checkBox.type = "checkbox"; // define o input do tipo checkbox
+	editarInput.type = "text"; // define o input do tipo text
+
+	botaoEditar.innerText = "Editar"; // cria o component em que dentro dele esta escrito Editar
+	botaoEditar.className = "edit"; // Cria uma class para esse componente que o nome dado foi edit
+	botaoDeletar.innerText = "Deletar"; // cria o component em que dentro dele esta escrito Deletar
+	botaoDeletar.className = "delete"; // Cria uma class para esse componente que o nome dado foi delete
+
+	/* comanod appendChild que cria um filho dentro de uma lista e joga ele por ultimo. Como nesse 
+	caso foi o listaDeItens que e um li que vai armazenar a lista dentro do html.
+	Logo ele vai armazenar tudo la em fileira de criacao seguida. */
+
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editarInput);
+	listItem.appendChild(botaoEditar);
+	listItem.appendChild(botaoDeletar);
+	return listaDeItens; // retorna a lista de itens atualizada
 
 }
 
 let adicionarTarefa = () => {
 	console.log(" Adicionar tarefa a lista de tarefas...");
+	
+
 }
 
 let editarTarefa = () => {
