3d4f80843e83efe292920cb87b1a55afcf6f034f
philipheferreira <philipheferreira@gmail.com>
2026-02-21 18:33:36 -0300

    Fisnih first project search

---
+++ 004 - projects/011 - search bar/001/script.js
@@ -0,0 +1,33 @@
+var nomes = [ "Harry", "Ross", "Bruce", "Cook", "Carolyn", "Morgan", "Albert", "WAlker", "Randy", "Reed", "Larry", "Barnes", "Lois", "Wilson", "Jesse", "Campbell", "Ernest", "Rogers", "Theresa", "Partterson", "Henry", "Simmons", "Michelle", "Perry", "Frank", "Butler", "Shirley" ];
+
+
+let outputdiv = document.querySelector(".output");
+
+let htmlstring = nomes.map(item => {
+	return `<h3> ${item}</h3>`
+}).join('')
+
+outputdiv.innerHTML = htmlstring
+
+let buscar = document.querySelector(".barraDeBuscador");
+
+buscar.addEventListener('keyup', (e) => {
+
+	var item = e.target.value;
+	var listaDeNomesPossiveisPelaBuscaAposDigitarLetras = [];
+	// console.log(e.target.value) // atualiza toda vez que termina de digitar por caracter
+	nomes.map( nome => {
+		if (nome.toLowerCase().includes(item.toLowerCase())){
+			listaDeNomesPossiveisPelaBuscaAposDigitarLetras.push(nome);
+			//console.log(nome) // Mostra todas as combinacoes com as letras atuais
+		} // toda letra sera minuscula
+	})
+	console.log(listaDeNomesPossiveisPelaBuscaAposDigitarLetras)
+
+	let htmlstring = listaDeNomesPossiveisPelaBuscaAposDigitarLetras.map(item => {
+		return `<h3> ${item}</h3>`
+	}).join('')
+
+	outputdiv.innerHTML = htmlstring;
+
+})
