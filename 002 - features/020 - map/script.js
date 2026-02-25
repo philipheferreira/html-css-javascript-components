8069acbacadfa420f89390969b801ef3c604ab2f
philipheferreira <philipheferreira@gmail.com>
2026-02-25 13:07:23 -0300

    Feat: little corrections for features past projects

--- 002 - features/020 - map/script.js
+++
@@ -1,9 +0,0 @@
-const itens = [ 
-{nome: 'Bicicleta', preco: 100 },
-{nome: 'TV', preco: 200 },
-{nome: 'Album', preco: 10 },
-{nome: 'Livro', preco: 5 },
-{nome: 'telefone', preco: 500 },
-{nome: 'Computador', preco: 1000 },
-{nome: 'teclado', preco: 25 }
-]
