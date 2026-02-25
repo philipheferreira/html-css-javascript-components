117fcc8592838bdf30d8f1fa69e6f1d5a802725e
philipheferreira <philipheferreira@gmail.com>
2026-02-25 12:18:54 -0300

    Feat: create basic layout in html and javascript constante

---
+++ 002 - features/022 - foreach/script.js
@@ -0,0 +1,9 @@
+const itens = [ 
+{nome: 'Bicicleta', preco: 100 },
+{nome: 'TV', preco: 200 },
+{nome: 'Album', preco: 10 },
+{nome: 'Livro', preco: 5 },
+{nome: 'telefone', preco: 500 },
+{nome: 'Computador', preco: 1000 },
+{nome: 'teclado', preco: 25 }
+]
