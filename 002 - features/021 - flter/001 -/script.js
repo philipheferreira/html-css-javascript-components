0b6424dfee19cfb98c0a29680f1c8f816ab63721
philipheferreira <philipheferreira@gmail.com>
2026-02-25 12:34:28 -0300

    Feat: filter project

--- 002 - features/021 - flter/001 -/script.js
+++ 002 - features/021 - flter/001 -/script.js
@@ -1,9 +1,17 @@
+// uma constante de array com objetos registrados
 const itens = [ 
-{nome: 'Bicicleta', preco: 100 },
-{nome: 'TV', preco: 200 },
-{nome: 'Album', preco: 10 },
-{nome: 'Livro', preco: 5 },
-{nome: 'telefone', preco: 500 },
-{nome: 'Computador', preco: 1000 },
-{nome: 'teclado', preco: 25 }
+	{nome: 'Bicicleta', preco: 100 },
+	{nome: 'TV', preco: 200 },
+	{nome: 'Album', preco: 10 },
+	{nome: 'Livro', preco: 5 },
+	{nome: 'telefone', preco: 500 },
+	{nome: 'Computador', preco: 1000 },
+	{nome: 'teclado', preco: 25 }
 ]
+
+let filtroListaDeItens = itens.filter((item) => {
+	return item.preco <= 100;
+})
+
+console.log(itens);
+console.log(filtroListaDeItens);
