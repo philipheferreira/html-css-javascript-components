218dd7ee7c0022cfef74a19579ee65f5157daeff
philipheferreira <philipheferreira@gmail.com>
2026-03-02 13:12:16 -0300

    Feat: make basic structs for project 32

--- 003 - pages/032 - form variables with validations and create infos in html/script.js
+++ 003 - pages/032 - form variables with validations and create infos in html/script.js
@@ -1,8 +1,11 @@
 let infosFormulario = document.querySelector('.meuFormulario');
 let submitBotao = document.querySelector(".botaoSubmit");
 
-let pressionarBotaoAtivar = () => {
+let clickarAtivarSubmitDoFormulario = () => {
+
+	event.preventDefault(); /* Impede o comportamento padrao do formulario, que seria recarregar a pagina */
+	
 	alert("Botao pressionado");
 }
 
-botaoTeste.addEventListener("click", pressionarBotaoAtivar);
+submitBotao.addEventListener("click", clickarAtivarSubmitDoFormulario);
