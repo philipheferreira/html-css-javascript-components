b6419f22523006f9c836c6cf7353e1253410b883
philipheferreira <philipheferreira@gmail.com>
2026-03-21 12:26:28 -0300

    Feat: validation operation

--- 003 - pages/022 - login page simple validation method/003 - Login page simple validation method 1, function 2/script.js
+++ 003 - pages/022 - login page simple validation method/003 - Login page simple validation method 1, function 2/script.js
@@ -24,14 +24,40 @@ let logarBotao = document.querySelector(".logar");
 logarBotao.addEventListener("click", function(event) {
 	event.preventDefault();	/* A linha mais importante: impede o envio e o recarregamento da pagina */
 
-	let login = document.querySelector('.login').value
-	let senha = document.querySelector('.senha').value
+	// Variaveis de valores dentro dos campos
+	let Valorlogin = document.querySelector('.login').value;
+	let Valorsenha = document.querySelector('.senha').value;
 
-	if(login == "admin" && senha == "admin"){
+	// campos input
+	let campoLogin = document.querySelector('.login');
+	let campoSenha = document.querySelector('.senha');
+
+	if(Valorlogin === "" && Valorsenha === ""){
+		alert('Por favor, não deixe o campo login e o senha vazios.');
+		campoLogin.classList.add('erro-validacao');
+		campoSenha.classList.add('erro-validacao');
+	}else
+
+	if(Valorlogin === "" && Valorsenha !== ""){
+		alert('por favor, preencha algum valor dentro do campo login.');
+		campoLogin.classList.add('erro-validacao');
+		campoSenha.classList.remove('erro-validacao');
+	}else
+
+	if(Valorlogin !== "" && Valorsenha === ""){
+		alert('Por favor, preencha algum valor dentro do campo senha.');
+		campoLogin.classList.remove('erro-validacao');
+		campoSenha.classList.add('erro-validacao');
+	}else
+	if(Valorlogin == "admin" && Valorsenha == "admin"){
 		alert('Bem vindo usuario admin, voce logou com sucesso.');
+		campoLogin.classList.remove('erro-validacao');
+		campoSenha.classList.remove('erro-validacao');
 		location.href = "pages/home.html";
 		
 	}else{
 		alert("Usuario ou senha incorretos.");
+		campoLogin.classList.remove('erro-validacao');
+		campoSenha.classList.remove('erro-validacao');
 	}
 } );
