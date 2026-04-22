d8157421d1e12d385add5a65b3b6778c86e167b5
philipheferreira <philipheferreira@gmail.com>
2026-04-22 08:19:27 -0300

    Feat: create new table project and adjust functions in simple table project

---
+++ 003 - pages/002 - minhas paginas/012 - table/005 - tabela insere elementos/script.js
@@ -0,0 +1,17 @@
+let formularioAdicionarNovaPessoa = document.querySelector('.formularioAdicionarNovaPessoa');
+
+
+let criarNovaPessoaTabela = () => {
+
+}
+
+
+let adicionarNovaPessoaTabela = (nome, idade, cargo) => {
+
+}
+
+let removerPessoaDaTabela = () => {
+
+}
+
+formularioAdicionarNovaPessoa.addEventListener('submit', criarNovaPessoaTabela());
