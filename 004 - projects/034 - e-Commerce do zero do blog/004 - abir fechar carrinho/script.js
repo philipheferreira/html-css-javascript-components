ddc01bac08a070693df40d462f239130fc7bf5a9
philipheferreira <philipheferreira@gmail.com>
2026-08-21 15:38:01 -0300

    final adjusts

--- 004 - projects/034 - e-Commerce do zero do blog/004 - abir fechar carrinho/script.js
+++ 004 - projects/034 - e-Commerce do zero do blog/004 - abir fechar carrinho/script.js
@@ -209,3 +209,150 @@ function closeModal() {
   body.classList.remove("modal-open");
 }
 
+// adicionar produtos ao carrinho
+
+
+let allProducts;
+let cart = []; // <= added code
+
+// function adds product to cart
+function addToCart(id) {
+  // find it product is in cart
+  const searchCart = cart.find((product) => product.id === id);
+  // disallows duplicate items to be put in cart
+  if (searchCart) {
+    alert("Product already added to the cart");
+  } else {
+    // if no product with same id in cart, add to cart
+    const oldProduct = allProducts.find((product) => product.id === id);
+    // push product to cart, along with quantity of 1, to increase/decrease number later in cart
+    cart.push({ ...oldProduct, quantity: 1 });
+  }
+  console.log(cart);
+
+  
+}
+
+
+function shoppingCart() {
+  const cartList = document.querySelector("#cart-list");
+  const cartTotal = document.querySelector("#cart-total");
+  const cartNumber = document.querySelector(".cart-number-container");
+  let cartHTML = "";
+
+  // map through cart, display cart contents
+  cart
+    .map((product) => {
+      const { title, price, quantity, id } = product;
+      cartHTML += `
+   <li id="item-container">
+    <div class="cart-title">
+      <h3>${title.substring(0, 30)}...</h3>
+    </div>
+    <div class="cart-quantity-container">
+     <p class="cart-price">$${price.toFixed(2)}</p>
+     <div class="button-quantity-container">
+      <button class="plus" onclick="increment(${id}, event)">+</button>
+      <p>${quantity}</p>
+      <button class="minus" onclick="decrement(${id}, event)">-</button>
+      </div>
+      <p id="item-total">$${(price * quantity).toFixed(2)}</p>
+     <div class="remove-cart-item">
+       <button onclick="deleteCartItem(${id}, event)">X</button>
+     </div>
+    </div>
+   </li>
+  `;
+    })
+    .join("");
+  cartList.innerHTML = cartHTML;
+
+  // select the total amount (quantity * price) of each item
+  const itemTotals = document.querySelectorAll("#item-total");
+  let sum = 0;
+  // iterate over all the items total price
+  itemTotals.forEach((itemTotal) => {
+    const numericValue = itemTotal.innerHTML;
+    // find where the price starts and cut the rest
+    const index = numericValue.indexOf("$");
+    // turn into a number value
+    sum += Number(numericValue.slice(index + 1));
+  });
+  // display depending on if the cart is empty or not
+  cartTotal.innerHTML =
+    sum > 0
+      ? `<button id="checkout" onclick="checkout()">Pay Now: $${sum.toFixed(
+          2
+        )}</button>`
+      : `No items in the cart`;
+
+  let newSum = 0;
+  cart.map((product) => {
+    newSum += product.quantity;
+  });
+
+  if (newSum < 1) {
+    cartNumber.classList.add("hide");
+  } else {
+    cartNumber.classList.remove("hide");
+    cartNumber.innerHTML = newSum;
+  }
+}
+
+
+// function adds product to cart
+function addToCart(id) {
+  // find it product is in cart
+  const searchCart = cart.find((product) => product.id === id);
+  // disallows duplicate items to be put in cart
+  if (searchCart) {
+    alert("Product already added to the cart");
+  } else {
+    // if no product with same id in cart, add to cart
+    const oldProduct = allProducts.find((product) => product.id === id);
+    // push product to cart, along with quantity of 1, to increase/decrease number later in cart
+    cart.push({ ...oldProduct, quantity: 1 });
+  }
+  console.log(cart);
+  // update shopping cart to show new product
+  shoppingCart(); // <= called function
+}
+
+function increment(id) {
+  const cartProduct = cart.find((product) => product.id === id);
+  // increase product quantity
+  if (cartProduct) {
+    cartProduct.quantity++;
+  }
+  // update cart after product quantity increment
+  shoppingCart();
+}
+
+// function to minus quantity from the product in the cart
+function decrement(id) {
+  const cartProduct = cart.find((product) => product.id === id);
+  // if quantity number higher than 1, decrement quantity
+  if (cartProduct && cartProduct.quantity > 1) {
+    cartProduct.quantity--;
+  }
+  // update cart
+  shoppingCart();
+}
+
+
+function deleteCartItem(id) {
+  cart = cart.filter((product) => product.id !== id);
+
+  // update cart
+  shoppingCart();
+}
+
+
+// function to leave purchase message and empty cart contents
+function checkout() {
+  // const checkoutBtn = document.querySelector("#checkout");
+  const cartList = document.querySelector("#cart-list");
+  cart = [];
+  shoppingCart();
+  cartList.innerHTML = `<p class="checkout-message">Thank you for your purchase</p>`;
+}
